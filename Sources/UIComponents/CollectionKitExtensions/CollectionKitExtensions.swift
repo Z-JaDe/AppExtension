746d2b3d39b712d1ee8a106bc004ae8132b5f75b
郑军铎 <zjade@outlook.com>
2018-12-12 19:30:06 +0800

    小修改

---
+++ Sources/UIComponents/CollectionKitExtensions/CollectionKitExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionKitExtensions.swift
+//  AppExtension
+//
+//  Created by 郑军铎 on 2018/12/12.
+//  Copyright © 2018 ZJaDe. All rights reserved.
+//
+
+import Foundation
+
+#if !AppExtensionPods
+@_exported import UIComponents
+#endif
---
+++ Sources/UIComponents/CollectionKitExtensions/PageScrollViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PageScrollViewController.swift
+//  AppExtension
+//
+//  Created by 郑军铎 on 2018/12/12.
+//  Copyright © 2018 ZJaDe. All rights reserved.
+//
+
+import Foundation
