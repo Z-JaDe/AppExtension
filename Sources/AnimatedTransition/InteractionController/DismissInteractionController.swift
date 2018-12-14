e0131b0399ab6bc0a2773e366ec396d7b3a365ca
郑军铎 <zjade@outlook.com>
2018-12-14 18:45:07 +0800

    小修改

--- Sources/AnimatedTransition/InteractionController/DismissInteractionController.swift
+++ Sources/AnimatedTransition/InteractionController/DismissInteractionController.swift
@@ -1,50 +1,50 @@
+////
+////  DismissInteractionController.swift
+////  PaiBaoTang
+////
+////  Created by 茶古电子商务 on 2017/7/27.
+////  Copyright © 2017年 Z_JaDe. All rights reserved.
+////
 //
-//  DismissInteractionController.swift
-//  PaiBaoTang
+//import UIKit
 //
-//  Created by 茶古电子商务 on 2017/7/27.
-//  Copyright © 2017年 Z_JaDe. All rights reserved.
+//public enum DismissDirection {
+//    case horizontal(leftToRight: Bool)
+//    case vertical(topToBottom: Bool)
+//}
 //
-
-import UIKit
-
-public enum DismissDirection {
-    case horizontal(leftToRight: Bool)
-    case vertical(topToBottom: Bool)
-}
-
-open class DismissInteractionController: InteractionController {
-    public var dismissDirection: DismissDirection = .vertical(topToBottom: true)
-
-    open override func handleGestureBegin(_ gesture: UIGestureRecognizer, _ view: UIView) {
-        guard let gesture = gesture as? UIPanGestureRecognizer else {
-            fatalError()
-        }
-        super.handleGestureBegin(gesture, view)
-        viewController?.dismiss(animated: true, completion: nil)
-    }
-    open override func updatePercent(_ gesture: UIGestureRecognizer, _ view: UIView) {
-        guard let gesture = gesture as? UIPanGestureRecognizer else {
-            fatalError()
-        }
-        let translation = gesture.translation(in: view)
-
-        let screenWidth = UIScreen.main.bounds.size.width
-        let screenHeight = UIScreen.main.bounds.size.height
-        switch self.dismissDirection {
-        case .horizontal:
-            percentValue = translation.x / screenWidth
-        case .vertical:
-            percentValue = translation.y / screenHeight
-        }
-
-        let velocity = gesture.velocity(in: view)
-        switch self.dismissDirection {
-        case .horizontal(leftToRight: let leftToRight):
-            self.shouldCompleteTransition = leftToRight == (velocity.x > 0)
-        case .vertical(topToBottom: let topToBottom):
-            self.shouldCompleteTransition = topToBottom == (velocity.y > 0)
-
-        }
-    }
-}
+//open class DismissInteractionController: InteractionController {
+//    public var dismissDirection: DismissDirection = .vertical(topToBottom: true)
+//
+//    open override func handleGestureBegin(_ gesture: UIGestureRecognizer, _ view: UIView) {
+//        guard let gesture = gesture as? UIPanGestureRecognizer else {
+//            fatalError()
+//        }
+//        super.handleGestureBegin(gesture, view)
+//        viewController?.dismiss(animated: true, completion: nil)
+//    }
+//    open override func updatePercent(_ gesture: UIGestureRecognizer, _ view: UIView) {
+//        guard let gesture = gesture as? UIPanGestureRecognizer else {
+//            fatalError()
+//        }
+//        let translation = gesture.translation(in: view)
+//
+//        let screenWidth = UIScreen.main.bounds.size.width
+//        let screenHeight = UIScreen.main.bounds.size.height
+//        switch self.dismissDirection {
+//        case .horizontal:
+//            percentValue = translation.x / screenWidth
+//        case .vertical:
+//            percentValue = translation.y / screenHeight
+//        }
+//
+//        let velocity = gesture.velocity(in: view)
+//        switch self.dismissDirection {
+//        case .horizontal(leftToRight: let leftToRight):
+//            self.shouldCompleteTransition = leftToRight == (velocity.x > 0)
+//        case .vertical(topToBottom: let topToBottom):
+//            self.shouldCompleteTransition = topToBottom == (velocity.y > 0)
+//
+//        }
+//    }
+//}
