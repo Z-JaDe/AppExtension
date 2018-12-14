5c0bd6c1adb3c23dd4e6c550f0fd81fe79dc89f5
郑军铎 <zjade@outlook.com>
2018-12-14 10:06:22 +0800

    bug

--- Sources/UIComponents/Core/Scroll/View/SegmentView.swift
+++ Sources/UIComponents/Core/Scroll/View/SegmentView.swift
@@ -86,10 +86,13 @@ open class SegmentView<ItemView, ItemData>: MultipleItemsView<ItemView, ItemData
     // MARK: -
     public var didSelectItem: ((TapContext<ItemView, ItemData>) -> Void)?
     @objc open func whenTap(_ tap: UITapGestureRecognizer) {
+        guard let didSelectItem = self.didSelectItem else {
+            return
+        }
         if let (offset, element) = self.cellArr.enumerated().first(where: {$0.element.point(inside: tap.location(in: $0.element), with: nil)}) {
             self.currentIndex = offset
             let context = TapContext(view: element, data: dataArray[offset], index: offset)
-            self.didSelectItem?(context)
+            didSelectItem(context)
         }
     }
 }
