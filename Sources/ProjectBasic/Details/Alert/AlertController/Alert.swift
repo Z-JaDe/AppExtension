db9f7d0c1c90436859ba792d542c9c25fbf2c0d9
mac <zjade@outlook.com>
2019-03-15 14:48:46 +0800

    AlertViewController

--- Sources/ProjectBasic/Details/Alert/AlertController/Alert.swift
+++ Sources/ProjectBasic/Details/Alert/AlertController/Alert.swift
@@ -103,3 +103,94 @@ extension Alert {
             }.show()
     }
 }
+
+import AudioToolbox
+extension UIAlertController {
+    convenience init(style: UIAlertController.Style, source: UIView? = nil, title: String? = nil, message: String? = nil, tintColor: UIColor? = nil) {
+        self.init(title: title, message: message, preferredStyle: style)
+        
+        let isPad: Bool = UIDevice.current.userInterfaceIdiom == .pad
+        let root = UIApplication.shared.keyWindow?.rootViewController?.view
+        
+        //self.responds(to: #selector(getter: popoverPresentationController))
+        if let source = source {
+            popoverPresentationController?.sourceView = source
+            popoverPresentationController?.sourceRect = source.bounds
+        } else if isPad, let source = root, style == .actionSheet {
+            popoverPresentationController?.sourceView = source
+            popoverPresentationController?.sourceRect = CGRect(x: source.bounds.midX, y: source.bounds.midY, width: 0, height: 0)
+            //popoverPresentationController?.permittedArrowDirections = .down
+            popoverPresentationController?.permittedArrowDirections = .init(rawValue: 0)
+        }
+        
+        if let color = tintColor {
+            self.view.tintColor = color
+        }
+    }
+    public func show(animated: Bool = true, vibrate: Bool = false, style: UIBlurEffect.Style? = nil, completion: (() -> Void)? = nil) {
+        if let style = style {
+            for subview in view.allSubViewsOf(type: UIVisualEffectView.self) {
+                subview.effect = UIBlurEffect(style: style)
+            }
+        }
+        DispatchQueue.main.async {
+            UIApplication.shared.keyWindow?.rootViewController?.present(self, animated: animated, completion: completion)
+            if vibrate {
+                AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+            }
+        }
+    }
+    func addAction(image: UIImage? = nil, title: String, color: UIColor? = nil, style: UIAlertAction.Style = .default, isEnabled: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
+        //let isPad: Bool = UIDevice.current.userInterfaceIdiom == .pad
+        //let action = UIAlertAction(title: title, style: isPad && style == .cancel ? .default : style, handler: handler)
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        action.isEnabled = isEnabled
+        
+        // button image
+        if let image = image {
+            action.setValue(image, forKey: "image")
+        }
+        
+        // button title color
+        if let color = color {
+            action.setValue(color, forKey: "titleTextColor")
+        }
+        addAction(action)
+    }
+    func set(title: String?, font: UIFont, color: UIColor) {
+        if title != nil {
+            self.title = title
+        }
+        setTitle(font: font, color: color)
+    }
+    
+    func setTitle(font: UIFont, color: UIColor) {
+        guard let title = self.title else { return }
+        let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: color]
+        let attributedTitle = NSMutableAttributedString(string: title, attributes: attributes)
+        setValue(attributedTitle, forKey: "attributedTitle")
+    }
+
+    func set(message: String?, font: UIFont, color: UIColor) {
+        if message != nil {
+            self.message = message
+        }
+        setMessage(font: font, color: color)
+    }
+    
+    func setMessage(font: UIFont, color: UIColor) {
+        guard let message = self.message else { return }
+        let attributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: color]
+        let attributedMessage = NSMutableAttributedString(string: message, attributes: attributes)
+        setValue(attributedMessage, forKey: "attributedMessage")
+    }
+    
+    func set(vc: UIViewController?, height: CGFloat? = nil) {
+        guard let vc = vc else { return }
+        setValue(vc, forKey: "contentViewController")
+        if let height = height {
+            vc.preferredContentSize.height = height
+            preferredContentSize.height = height
+        }
+    }
+}
