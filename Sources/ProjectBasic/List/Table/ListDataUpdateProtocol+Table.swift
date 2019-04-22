cf22e747da87d4f17584bae6efb4acba0aee1a93
Apple <zjade@outlook.com>
2019-04-22 20:58:09 +0800

    列表刷新 api 简化

--- Sources/ProjectBasic/List/Table/ListDataUpdateProtocol+Table.swift
+++ Sources/ProjectBasic/List/Table/ListDataUpdateProtocol+Table.swift
@@ -13,6 +13,24 @@ extension ListDataUpdateProtocol where Item == AnyTableAdapterItem {
     public func reloadData(_ listCellData: ListData<Section, StaticTableItemCell>?) {
         self.reloadData(listCellData?.map({.cell($0)}))
     }
+    /// ZJaDe: 重新刷新cell
+    public func reloadData(_ listModelData: ListData<Section, TableItemModel>?) {
+        self.reloadData(listModelData?.map({.model($0)}))
+    }
+}
+extension ListDataUpdateProtocol where Section: Equatable & InitProtocol, Item == AnyTableAdapterItem {
+    /// ZJaDe: 重新刷新 返回 ListUpdateInfoType
+    public func reloadData(section: Section? = nil, _ itemArray: [TableItemModel]?, isRefresh: Bool, _ closure: ((ListUpdateInfo<ListDataType>) -> (ListUpdateInfo<ListDataType>))? = nil) {
+        let _itemArray: [AnyTableAdapterItem] = itemArray?.map({.model($0)}) ?? []
+        var newData = self.dataArray
+        if isRefresh {
+            newData.reset(section: section, items: _itemArray)
+        } else if _itemArray.count > 0 {
+            newData.append(section: section, items: _itemArray)
+        }
+        let result = newData.updateInfo()
+        self.reloadData(closure?(result) ?? result)
+    }
 }
 extension ListData where Item: StaticTableItemCell, Section == TableSection {
     public func updateInfo() -> TableListUpdateInfo {
