73b0a62b72e75f85eef3b1f68e467c84b414c461
zjade <zjade@outlook.com>
2019-08-12 18:05:25 +0800

    AppExtension Playground

---
+++ Playground_Map.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: A MapKit based Playground
+
+import MapKit
+import PlaygroundSupport
+
+
+let appleParkWayCoordinates = CLLocationCoordinate2DMake(37.334922, -122.009033)
+
+// Now let's create a MKMapView
+let mapView = MKMapView(frame: CGRect(x:0, y:0, width:800, height:800))
+
+// Define a region for our map view
+var mapRegion = MKCoordinateRegion()
+
+let mapRegionSpan = 0.02
+mapRegion.center = appleParkWayCoordinates
+mapRegion.span.latitudeDelta = mapRegionSpan
+mapRegion.span.longitudeDelta = mapRegionSpan
+
+mapView.setRegion(mapRegion, animated: true)
+
+// Create a map annotation
+let annotation = MKPointAnnotation()
+annotation.coordinate = appleParkWayCoordinates
+annotation.title = "Apple Inc."
+annotation.subtitle = "One Apple Park Way, Cupertino, California."
+
+mapView.addAnnotation(annotation)
+
+// Add the created mapView to our Playground Live View
+PlaygroundPage.current.liveView = mapView
